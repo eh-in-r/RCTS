0f26302abd0827a6636c5d8a874be07e2d4b561c
EH <ewoud.heyndels@vub.be>
2022-05-18 11:57:11 +0200

    .

--- R/functions_parallel.R
+++ R/functions_parallel.R
@@ -146,6 +146,7 @@ make_df_pic_parallel <- function(x) {
 #' @inheritParams calculate_VCsquared
 #' @export
 parallel_algorithm <- function(original_data, indices_subset, S_cand, k_cand, kg_cand, C_candidates, USE_DO = FALSE) {
+  df_results_full <- NULL
   rc <- initialise_rc(indices_subset, C_candidates) # dataframe that will contain the optimized number of common factors for each C and subset
   rcj <- initialise_rcj(indices_subset, C_candidates) # dataframe that will contain the optimized number of groups and group specific factors for each C and subset
 
@@ -186,23 +187,28 @@ parallel_algorithm <- function(original_data, indices_subset, S_cand, k_cand, kg
         run_config(configs[i,], C_candidates, Y, X, maxit = 2)
       }
     }
+    print("foreach has finished")
+    print(output)
 
     df_results <- make_df_results_parallel(output)
     df_pic <- make_df_pic_parallel(output)
 
-
+    print("_1")
     pic_sigma2 <- df_results$sigma2[nrow(df_results)]
     df_pic <- adapt_pic_with_sigma2maxmodel(df_pic, df_results, pic_sigma2)
 
 
+    print("_2")
     # calculate for each candidate value for C the best S, k and kg
     all_best_values <- calculate_best_config(df_results, df_pic, C_candidates)
     rc <- fill_rc(rc, all_best_values, subset) # best number of common factors
     rcj <- fill_rcj(rcj, all_best_values, subset, S_cand, kg_cand) # best number of group specific factors and groups
     rm(all_best_values)
 
+    print("_3")
     # keep df_results of the full sample (this will contain the final clustering):
     if (subset == 0) df_results_full <- df_results
   } # closes loop over subsets
+  print("einde")
   return(list(rc, rcj, df_results_full))
 }
