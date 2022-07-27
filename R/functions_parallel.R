56f44e80bae90a7485a1d18d8c7c28b6593788a8
EH <ewoud.heyndels@vub.be>
2022-07-27 17:45:33 +0200

    .

--- R/functions_parallel.R
+++ R/functions_parallel.R
@@ -287,7 +287,7 @@ parallel_algorithm <- function(original_data, indices_subset, S_cand, k_cand, kg
           df_pic <- list()
           for( i in 1:length(choice_pic) ) {
             #remaining issue here:
-            print(output %>% purrr::map(4))
+            #print(output %>% purrr::map(4))
             df_pic[[i]] <- t(matrix(unlist(output %>% purrr::map(4) %>% purrr::map(i)), nrow = length(C_candidates)))
 
             df_pic[[i]] <- adapt_pic_with_sigma2maxmodel(df_pic[[i]], df_results, pic_sigma2)
